5d2b93b206c7e008bb86040ca953c2d037abbb97
janikRafal <rafal.jaanik@wp.pl>
2022-09-28 22:40:10 +0200

    tak

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,22 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import { createServer } from 'http';
+import socketIo, { Server } from 'socket.io';
 
 import { healthCheck } from 'routes/health-check';
+import { countReset } from 'console';
 type Message = {
     content: string;
     author: string;
     timestamp: string;
+    id: string;
 };
 const messages: Message[] = [
     {
         content: 'abc',
         author: 'Rafał',
         timestamp: '123',
+        id: '1',
     },
 ];
 
@@ -24,10 +29,6 @@ const port = process.env.PORT ?? 4000;
 
 app.get('/health-check', healthCheck);
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}.`);
-});
-
 app.get('/messages', function (_req, res) {
     res.send(messages);
 });
@@ -35,4 +36,21 @@ app.get('/messages', function (_req, res) {
 app.post('/send', (req, res) => {
     messages.push(req.body as Message);
     res.send('Message saved.');
+});
+
+const httpServer = createServer(app);
+const io = socketIo(httpServer);
+
+io.on(`connection`, (socket) => {
+    console.log(`client connected: `, socket.id);
+
+    socket.join(`clock-room`);
+
+    socket.on(`disconnect`, (reason) => {
+        console.log(reason);
+    });
+});
+
+httpServer.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
