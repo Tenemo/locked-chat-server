f43b2a5d7e43016768812762baa54cedc3be336c
Garrow <95480667+janikRafal@users.noreply.github.com>
2022-11-02 21:24:37 +0100

    Username (#6)
    * username v1.0
    
    * replaced user[] to user{}
    
    fixed set-username event
    
    fixed new-message event
    
    * fixed TypeScript problems
    
    * console.logs removed, small changes in names of variables
    
    * file with types added
    
    history object deleted (now object with users and messages is passed into emit directly)
    
    new events added
    
    now new messages are sending only to existing sockets
    
    * this time actually file with types added
    
    * small changes

--- src/server.ts
+++ src/server.ts
@@ -5,21 +5,34 @@ import { Server } from 'socket.io';
 
 import { healthCheck } from 'routes/health-check';
 import crypto from 'crypto';
+import { Events, Message, User } from 'types/types';
 
-type Message = {
-    content: string;
-    author: string;
-    timestamp: string;
-    id: string;
-};
 const messages: Message[] = [
     {
-        content: 'abc',
-        author: 'Rafał',
-        timestamp: '123',
-        id: '1',
+        content: 'Hi 1',
+        author: 'Test1',
+        timestamp: '2022-10-25T10:42:36.370Z',
+        id: 'cc9811c5217c525b411bfe7c8b616852a752822ca4de8d28b2b113b906a89824',
+    },
+    {
+        content: 'Hi 2',
+        author: 'Test2',
+        timestamp: '2022-10-25T10:45:11.390Z',
+        id: '616852a752822ca4cc98112b113b906a8c5217c525b411bfe7c8bde8d28b9824',
+    },
+    {
+        content: 'Hi 3',
+        author: 'Test3',
+        timestamp: '2022-10-25T10:48:36.150Z',
+        id: 'de8d28b2b113b90bfe7c8b6168cc9811c5217c525b41152a752822ca46a89824',
     },
 ];
+
+const users: User = {
+    _E9JSqynu35fJ4bIAAAB: 'Test1',
+    znlbFaz31OAxAX7RAAAD: 'Test2',
+    znlbFaz31OAxAX7RDDAD: 'Test3',
+};
 
 dotenv.config();
 
@@ -38,23 +51,47 @@ const io = new Server(httpServer, {
 });
 
 io.on(`connection`, async (socket) => {
-    socket.send(messages);
     console.log(`client connected: `, socket.id);
 
-    socket.on('new-message', (data: string) => {
-        console.log(data);
+    socket.on(Events.SET_USERNAME, (username: string) => {
+        if (
+            Object.values(users).some(
+                (existingUsername) => existingUsername === username,
+            )
+        ) {
+            socket.emit(Events.SET_USERNAME_FAILURE, username);
+        } else {
+            users[socket.id] = username;
+
+            socket.emit(Events.SET_USERNAME_SUCCESS, {
+                messages: messages,
+                usernames: Object.values(users),
+            });
+        }
+    });
+
+    socket.on(Events.NEW_MESSAGE, (content: string) => {
+        const author = users[socket.id];
+
+        if (!author) {
+            socket.emit(Events.NEW_MESSAGE_USERNAME_NOT_REGISTERED);
+            return;
+        }
+
         const timestamp = new Date().toISOString();
         const id = crypto.randomBytes(32).toString('hex');
 
         const newMessage: Message = {
-            content: data,
-            author: socket.id,
+            content,
+            author,
             timestamp,
             id,
         };
 
         messages.push(newMessage);
-        io.emit('new-message-update', newMessage);
+        Object.keys(users).forEach((key) =>
+            io.to(key).emit(Events.NEW_MESSAGE_UPDATE, newMessage),
+        );
     });
 
     socket.on(`disconnect`, (reason) => {
---
+++ src/types/types.ts
@@ -0,0 +1,17 @@
+export enum Events {
+    NEW_MESSAGE = 'new-message',
+    NEW_MESSAGE_UPDATE = 'new-message-update',
+    NEW_MESSAGE_USERNAME_NOT_REGISTERED = 'new-message-username-not-registered',
+    SET_USERNAME = 'set-username',
+    SET_USERNAME_SUCCESS = 'set-username-success',
+    SET_USERNAME_FAILURE = 'set-username-failure',
+}
+
+export type Message = {
+    content: string;
+    author: string;
+    timestamp: string;
+    id: string;
+};
+
+export type User = Record<string, string>;
